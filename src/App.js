2c02a2b0f73f9af9c1075879961dc3e9649caa27
melvinsamuel070 <melvinsamuel070@gmail.com>
2025-07-17 09:00:37 +0100

    Update message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          🚀 Hello from Melvin’s CI/CD App — now deploying automatically!
         </p>
         <a
           className="App-link"
